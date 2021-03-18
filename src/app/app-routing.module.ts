66aff4d2333a400fceace68c8f516ce9edcb9b34
bzaugt <tobiasluza@gmail.com>
2021-03-18 09:43:04 +0100

    update repo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { GameComponent } from './game/game.component';
 import { LocationComponent } from './location/location.component';
 
 const routes: Routes = [
+  { path: '', component: LocationComponent },
   { path: 'location', component: LocationComponent },
   { path: 'game', component: GameComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
